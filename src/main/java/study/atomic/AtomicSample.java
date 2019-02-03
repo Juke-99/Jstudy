6208bb3f593d2ebe822ab5c418f33a186fe19fb4
Juke-99 <yanpahika@gmail.com>
2019-02-04 04:08:20 +0900

    compare atomicinteger and integer process

--- src/main/java/study/atomic/AtomicSample.java
+++ src/main/java/study/atomic/AtomicSample.java
@@ -1,5 +1,6 @@
 package study.atomic;
 
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -7,6 +8,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class AtomicSample {
+	HashMap<Integer, Integer> a = new HashMap<>();
+	
 	public int atomicInteger() {
 		AtomicInteger atInt = new AtomicInteger();
 		ExecutorService executor = Executors.newFixedThreadPool(2);
@@ -15,6 +18,18 @@ public class AtomicSample {
 		stop(executor);
 		
 		return atInt.get();
+	}
+	
+	// I don't know that different between Integer and AtomicInteger.
+	@SuppressWarnings("deprecation")
+	public int nonAtomicInteger() {
+		Integer integer = new Integer(100);
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		IntStream.range(0, 100).forEach(i -> executor.submit(integer::intValue));
+		
+		stop(executor);
+		
+		return integer;
 	}
 	
 	public void stop(ExecutorService executor) {
--- src/test/java/study/atomic/AtomicSampleTest.java
+++ src/test/java/study/atomic/AtomicSampleTest.java
@@ -2,13 +2,20 @@ package study.atomic;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.logging.Logger;
+
 import org.junit.Test;
 
 public class AtomicSampleTest {
+	Logger logger = Logger.getLogger("AtomicSampleTest");
 	AtomicSample atomics = new AtomicSample();
 	
 	@Test
 	public void testAtomicInteger() {
+		logger.info("atomic start");
 		assertEquals(100, atomics.atomicInteger());
+		
+		logger.info("non atomic start");
+		assertEquals(100, atomics.nonAtomicInteger());
 	}
 }
