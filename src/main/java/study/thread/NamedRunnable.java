3898668e3b97337d7e48d8ee88f2127e35df6011
Juke-99 <yanpahika@gmail.com>
2019-02-18 21:11:10 +0900

    forget commi

--- src/main/java/study/thread/NamedRunnable.java
+++ src/main/java/study/thread/NamedRunnable.java
@@ -1,10 +1,28 @@
 package study.thread;
 
-public class NamedRunnable implements Runnable {
+import java.util.Locale;
+import java.util.logging.Logger;
 
+public abstract class NamedRunnable implements Runnable {
+	private static final Logger logger = Logger.getLogger(NamedRunnable.class.getName());
+	final String name;
+
+	public NamedRunnable(String name, Object... args) {
+		this.name = String.format(Locale.US, name, args);
+	}
+	
 	@Override
 	public void run() {
+		logger.info("run server");
+		String previousThread = Thread.currentThread().getName();
+		Thread.currentThread().setName(name);
 		
+		try {
+			execute();
+		} finally {
+			Thread.currentThread().setName(previousThread);
+		}
 	}
-
+	
+	protected abstract void execute();
 }
--- src/main/java/study/thread/SimpleServer.java
+++ src/main/java/study/thread/SimpleServer.java
@@ -4,25 +4,55 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.net.ServerSocketFactory;
 
 public class SimpleServer {
 	private static final Logger logger = Logger.getLogger(SimpleServer.class.getName());
+	static int port;
 	
 	public void start() throws IOException {
 		InetSocketAddress inetSocketAddress = new InetSocketAddress(InetAddress.getByName("localhost"), 0);
 		ExecutorService executor = Executors.newCachedThreadPool(Util.threadFactory("SimpleServer", false));
 		ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket();
-		
-		logger.info("Server Port " + inetSocketAddress.getPort());
-		
 		serverSocket.setReuseAddress(false);
 		serverSocket.bind(inetSocketAddress, 10);
 		
-		executor.execute(new NamedRunnable());
+		port = serverSocket.getLocalPort();
+		
+		logger.info("Server Port " + port);
+		
+		executor.execute(new NamedRunnable("SimpleServer %s", port) {
+			@Override
+			protected void execute() {
+				try {
+					logger.info(SimpleServer.this + "start accept");
+					accept();
+				} catch (Throwable e) {
+					logger.log(Level.WARNING, SimpleServer.this + " failed unexpectedly", e);
+				}
+			}
+			
+			private void accept() throws Exception {
+				Socket socket;
+				
+				try {
+					socket = serverSocket.accept();
+				} catch (SocketException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	@Override
+	public String toString() {
+		return "SimpleServer port: " + port + " => ";
 	}
 }
